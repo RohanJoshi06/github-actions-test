22ca20b208fd32bcabc29a008dc4f2cd8335fa4a
RohanJoshi28 <rjoshi6@gmail.com>
2022-10-06 15:09:58 -0400

    add another path to api_linter.py

--- .github/workflows/api_linter.py
+++ .github/workflows/api_linter.py
@@ -1,6 +1,6 @@
 import ruamel.yaml
 
-with open("../../manifests/api-eks/alb/prd/api-public.yaml", "r") as f:
+with open("manifests/api-eks/alb/prd/api-public.yaml", "r") as f:
     yaml_parser = ruamel.yaml.YAML()
     data = yaml_parser.load(f.read())
 
